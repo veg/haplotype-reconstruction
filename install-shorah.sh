53b0d43ae1f9107d540d5f7a9816b1451e529c3a
hongs <tug67936@temple.edu>
2018-05-25 16:45:41 -0400

    add commands to shorah install script

--- install-shorah.sh
+++ install-shorah.sh
@@ -1,1 +1,60 @@
-test
+installDir=$(pwd)
+
+virtualenv shorah
+
+cd shorah
+
+#echo "$installDir"
+
+shorahDir=$(pwd)
+
+source bin/activate
+
+pip install numpy Biopython
+
+wget https://github.com/cbg-ethz/shorah/releases/download/v1.1.3/shorah-1.1.3.tar.bz2
+
+bunzip2 shorah-1.1.3.tar.bz2
+
+tar -xvf shorah-1.1.3.tar
+
+rm shorah-1.1.3.tar
+
+cd shorah-1.1.3
+
+configDir=$(pwd)
+
+configCommand="./configure --prefix="$shorahDir" PYTHON="$shorahDir"/bin/python
+
+make -j4
+
+make install
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
