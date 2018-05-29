b636866a1978e651eb9628f97eee3c631f8a0e8d
hongs <39339031+hongs@users.noreply.github.com>
2018-05-29 17:57:23 -0400

    Add ShoRAH install script (#4)
    * Initial commit
    
    * make file non-empty
    
    * add commands to shorah install script
    
    * Added line to make configure script run
    
    * add installation instructions
    
    * add message following install completion
    
    * Remove redundant path references
    
    * Add LICENSE
    
    * Add .gitignore, modify exit message

---
+++ install-shorah.sh
@@ -0,0 +1,38 @@
+#  This script will install ShoRAH into your current directory.
+#  to run it, type
+#
+#    bash install-shorah.sh
+#
+#  The program virtualenv needs to be installed prior to running this script.
+#  A new directory named 'shorah' will be created.
+#  ShoRAH's python script library will be created in ./shorah/bin
+
+virtualenv shorah
+
+cd shorah
+
+installDir=$(pwd)
+
+source bin/activate
+
+pip install numpy Biopython
+
+wget https://github.com/cbg-ethz/shorah/releases/download/v1.1.3/shorah-1.1.3.tar.bz2
+
+bunzip2 shorah-1.1.3.tar.bz2
+
+tar -xvf shorah-1.1.3.tar
+
+rm shorah-1.1.3.tar
+
+cd shorah-1.1.3
+
+configCommand="./configure --prefix="$installDir" PYTHON="$installDir"/bin/python"
+
+eval "$configCommand"
+
+make -j4
+
+make install
+
+printf "\n  ShoRAH 1.1.3 install script finished.\n\n"
